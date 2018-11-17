d45e03bae0353cd46702ec80a32d38a5e7261573
PlayingNumbers <kjee@scoutsconsulting.com>
2018-11-17 14:10:12 -0600

    Added notes

--- NBAGameSimulator.py
+++ NBAGameSimulator.py
@@ -35,12 +35,16 @@ class Team:
         return np.std(self.opp_points)
 
 class MatchupSimulator:
+    """ Simulates single game outcomes as well as multiple outcomes"""
     def __init__(self, Team1,Team2):
         self.Team1 = Team1
         self.Team2 = Team2
         self.results = []
-        
+    
+    #Simulates a single game returns 1 if team 1 wins, returns -1 if Team 2 wins, and returns 0 if the game is tied
     def gameSim(self):
+        #Averages the random sample of a teams points with a random sample of the number of points the opponent allows
+        #Randomly samples from the two gaussian distributions to produce a probabilistic outcome
         T1 = (rnd.gauss(self.Team1.pointsavg(),self.Team1.pointsstd())+ rnd.gauss(self.Team2.opp_pointsavg(),self.Team2.opp_pointsstd()))/2
         T2 = (rnd.gauss(self.Team2.pointsavg(),self.Team2.pointsstd())+ rnd.gauss(self.Team1.opp_pointsavg(),self.Team1.opp_pointsstd()))/2
         if int(round(T1)) > int(round(T2)):
@@ -55,6 +59,7 @@ class MatchupSimulator:
         team2win = 0
         tie = 0
         for i in range(number_simulations):
+            #calls the pervious game simulator and aggregates results
             gm = self.gameSim()
             gamesout.append(gm)
             if gm == 1:
@@ -65,6 +70,8 @@ class MatchupSimulator:
         print(self.Team1.TeamName + ' Win ', team1win/(team1win+team2win+tie),'%')
         print(self.Team2.TeamName + ' Win ', team2win/(team1win+team2win+tie),'%')
         print('Tie ', tie/(team1win+team2win+tie), '%')
+        print('Number of Simulations: ',number_simulations)
+        #can see all results using self.results
         self.results = gamesout
 
     
